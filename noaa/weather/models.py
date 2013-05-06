e22cd2ecd3906590ba147b5aa497830c0cc45afb
David Arbuckle <dintlu@gmail.com>
2013-05-06 09:26:04 -0600

    Tweaking database constraints.
    Signed-off-by: David Arbuckle <dintlu@gmail.com>

--- noaa/weather/models.py
+++ noaa/weather/models.py
@@ -5,7 +5,7 @@ class WBAN(models.Model):
     """
     WBAN is the NOAA Station ID.  This table can be updated with detailed station information if a source is found.
     """
-    wban = models.CharField(max_length=5)
+    wban = models.CharField(max_length=5, unique=True)
 
 
 class Weather(models.Model):
@@ -14,8 +14,8 @@ class Weather(models.Model):
     This is a 6-digit code that may contain a + or - modifier, which is applied in the join table.
     Any given report can have multiple concurrent conditions.
     """
-    code = models.CharField(max_length=6)
-    name = models.CharField(max_length=32)
+    code = models.CharField(max_length=8, unique=True)
+    name = models.CharField(max_length=32, null=True, blank=True)
 
 
 class Report_Weather(models.Model):
@@ -24,7 +24,6 @@ class Report_Weather(models.Model):
     """
     weather = models.ForeignKey(Weather)
     report = models.ForeignKey('Report')
-    modifier = models.CharField(max_length=1) # valid values: '-', '+', ' '
 
 
 class Report(models.Model):
@@ -34,15 +33,15 @@ class Report(models.Model):
     wban = models.ForeignKey(WBAN)
     date = models.DateTimeField()
     weather_type = models.ManyToManyField(Weather, through=Report_Weather)
-    visibility = models.DecimalField(max_digits=5, decimal_places=2) # visibility in ?miles?
+    visibility = models.DecimalField(null=True, max_digits=5, decimal_places=2) # visibility in ?miles?
     temp_dry = models.IntegerField() # dry bulb temp in Fahrenheit
-    temp_wet = models.IntegerField() # wet bulb temp in Fahrenheit
-    dew_point = models.IntegerField() # Fahrenheit dew point
-    humidity = models.IntegerField() # Relative humidity
-    wind_speed = models.IntegerField() # in knots.
-    wind_direction = models.IntegerField() # 000-360
-    pressure = models.DecimalField(max_digits=4, decimal_places=2) # Pressure in ?unknown unit?
-    precipitation = models.DecimalField(max_digits=5, decimal_places=2) # Hourly precip in inches
+    temp_wet = models.IntegerField(null=True) # wet bulb temp in Fahrenheit
+    dew_point = models.IntegerField(null=True) # Fahrenheit dew point
+    humidity = models.IntegerField(null=True) # Relative humidity
+    wind_speed = models.IntegerField(null=True) # in knots.
+    wind_direction = models.IntegerField(null=True) # 000-360
+    pressure = models.DecimalField(null=True, max_digits=4, decimal_places=2) # Pressure in ?unknown unit?
+    precipitation = models.DecimalField(null=True, max_digits=5, decimal_places=2) # Hourly precip in inches
 
 
 # TODO:  create sets from data for each of these data points in order to determine the structure of the data and ensure that the model accounts for all possible variations.
